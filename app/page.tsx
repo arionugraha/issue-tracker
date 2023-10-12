2c4ab74ca93472b0ee5bc5c74bb076bc88c47cb7
Ario Nugraha Ramadhanu <arionugraha5@gmail.com>
2023-10-13 00:47:44 +0700

    Build the new issue page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import Image from "next/image";
 export default function Home() {
    return (
       <>
-         <div>
-            <Button>New Issue</Button>
-         </div>
+         <div></div>
       </>
    );
 }
