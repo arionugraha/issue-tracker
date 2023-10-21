e7dcd64156fbd0fe81a6f1d24c6f8847abd79479
Ario Nugraha Ramadhanu <arionugraha5@gmail.com>
2023-10-22 02:43:53 +0700

    Add router refresh after adding new issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-import "easymde/dist/easymde.min.css";
-import { useForm, Controller, set } from "react-hook-form";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { AiOutlineWarning } from "react-icons/ai";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
-import z from "zod";
 import FormError from "@/app/components/FormError";
 import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { AiOutlineWarning } from "react-icons/ai";
+import z from "zod";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
 
@@ -43,6 +43,7 @@ const NewIssuePage = () => {
          console.log(res);
       } else {
          router.push("/issues");
+         router.refresh();
       }
    }
 
